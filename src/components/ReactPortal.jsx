b80513ec3aa76658d487054f423822ff4392b4b8
pkbogovic <pkbogovic@gmail.com>
2024-02-07 10:51:39 +0100

    Prepared build files for github pages

--- src/components/ReactPortal.jsx
+++
@@ -1,8 +0,0 @@
-import { createPortal } from "react-dom";
-
-const ReactPortal = ({children, id}) => {
-    const portalRoot = document.getElementById(id);
-    return createPortal(children, portalRoot);
-}
-
-export default ReactPortal;
